eda5dc4fb61955dcea18704c4e75dbca33e65670
randy-ang <randyanggadinata@gmail.com>
2019-07-06 16:34:58 +0700

    prop types

--- src/Denominator.js
+++ src/Denominator.js
@@ -1,20 +1,33 @@
-import React, {useEffect, useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {Field} from 'redux-form';
 import { TextField } from './util/field.util';
-import Button from '@material-ui/core/Button';
 import noop from 'lodash';
 import { onEnter } from './util/eventsHandler.util';
 import './Denominator.css';
 import LanguageContext from './context/language.context';
-import styles from './Denominator.styles';
+import PropTypes from 'prop-types';
 
 export const fields = {
   formName: 'denominatorForm',
   denominator: 'denominator',
 }
 
-const Denominator = ({handleSubmit = noop}) => {
+const customFocus = (setFocused, onFocus) => (event) => {
+  // when focused, Rp will appear
+  setFocused(true);
+  onFocus(event);
+}
+
+const customBlur = (setFocused, onBlur) => (event) => {
+  if(event.target.value.length === 0) { // if there are no value, then the Rp disappears
+    setFocused(false);
+}  
+  onBlur(event);
+}
+
+const Denominator = ({handleSubmit = noop, onFocus = noop, onBlur = noop}) => {
   const lang = useContext(LanguageContext);
+  const [focused, setFocused] = useState(false);
   return (
     <div className={'denomField'}>
       <Field
@@ -23,11 +36,20 @@ const Denominator = ({handleSubmit = noop}) => {
         component={TextField}
         label={lang.LABEL.VALUE}
         placeholder={lang.PLACEHOLDER.VALUE}
-        InputProps={{className: 'textField', startAdornment: <Currency/>}}
+        InputProps={{className: 'textField', startAdornment: focused && <Currency/>}}
+        onFocus={customFocus(setFocused, onFocus)}
+        onBlur={customBlur(setFocused, onBlur)}
       />
     </div>
   )
 }
+
+Denominator.propTypes = {
+  handleSubmit: PropTypes.func,
+  onFocus: PropTypes.func,
+  onBlur: PropTypes.func
+}
+
 
 const Currency = () => {
   const {RP} = useContext(LanguageContext);
--- src/Denominator.page.js
+++ src/Denominator.page.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './Denominator.css';
-import Button from '@material-ui/core/Button';
 import {reduxForm} from 'redux-form';
 import {connect} from 'react-redux';
 import Denominator, { fields } from './Denominator';
 import { validateFields, validateCurrency } from './util/validator.util';
 import { denominateValue } from './thunks/denominator.thunks';
 import Display from './Display.page';
-import { isEmptyOrNull, parseIndonesianCurrency } from './util/common.util';
+import { isEmptyOrNull } from './util/common.util';
+import PropTypes from 'prop-types';
 
 const formConfig = {
   form: fields.formName,
   onSubmit: ({[fields.denominator]: values}, dispatch, {history}) => {
     dispatch(denominateValue(values, history));
   },
-  validate: (values, props) => ({
+  validate: (values) => ({
     ...validateFields(values, [fields.denominator]), // to check against empty values
     [fields.denominator]: validateCurrency(values[fields.denominator])
     // [fields.name.username]: validateEmail(values[fields.name.username]),
@@ -35,6 +34,10 @@ const DenominatorPage = ({value, ...props}) => {
     </div>
     </div>
   );
+}
+
+DenominatorPage.propTypes = {
+  value: PropTypes.array,
 }
 
 const denomState = ({value}) => ({
