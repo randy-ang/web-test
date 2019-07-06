cec29965f916344ca3c29f9209cea2bc31f7e8da
randy-ang <randyanggadinata@gmail.com>
2019-07-06 13:27:48 +0700

    main denominator page

--- src/Denominator.js
+++ src/Denominator.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useEffect, useContext} from 'react';
 import {Field} from 'redux-form';
 import { TextField } from './util/field.util';
 import Button from '@material-ui/core/Button';
 import noop from 'lodash';
+import { onEnter } from './util/eventsHandler.util';
+import './Denominator.css';
+import LanguageContext from './context/language.context';
+import styles from './Denominator.styles';
 
 export const fields = {
   formName: 'denominatorForm',
@@ -10,19 +14,24 @@ export const fields = {
 }
 
 const Denominator = ({handleSubmit = noop}) => {
-  
+  const lang = useContext(LanguageContext);
   return (
-    <div>
-    <Field
-      name={fields.denominator}
-      component={TextField}
-      label={'Hello World'}
-    />
-    <Button variant="contained" color="primary" onClick={handleSubmit}>
-        Primary
-      </Button>
+    <div className={'denomField'}>
+      <Field
+        onKeyDown={onEnter(handleSubmit)}
+        name={fields.denominator}
+        component={TextField}
+        label={lang.LABEL.VALUE}
+        placeholder={lang.PLACEHOLDER.VALUE}
+        InputProps={{className: 'textField', startAdornment: <Currency/>}}
+      />
     </div>
   )
+}
+
+const Currency = () => {
+  const {RP} = useContext(LanguageContext);
+  return <section className="currencyContainer">{RP}</section>
 }
 
 export default Denominator;
--- src/Denominator.page.js
+++ src/Denominator.page.js
@@ -1,31 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './Denominator.css';
 import Button from '@material-ui/core/Button';
 import {reduxForm} from 'redux-form';
 import {connect} from 'react-redux';
 import Denominator, { fields } from './Denominator';
-import { validateFields } from './util/validator.util';
+import { validateFields, validateCurrency } from './util/validator.util';
+import { denominateValue } from './thunks/denominator.thunks';
+import Display from './Display.page';
+import { isEmptyOrNull, parseIndonesianCurrency } from './util/common.util';
 
 const formConfig = {
   form: fields.formName,
-  onSubmit: (values, dispatch, props) => {
-    console.log('values', values);
+  onSubmit: ({[fields.denominator]: values}, dispatch, {history}) => {
+    dispatch(denominateValue(values, history));
   },
-  // validate: (values, props) => ({
-  //   // ...validateFields(values, [fields.name.username, fields.name.password]),
-  //   // [fields.name.username]: validateEmail(values[fields.name.username]),
-  // })
+  validate: (values, props) => ({
+    ...validateFields(values, [fields.denominator]), // to check against empty values
+    [fields.denominator]: validateCurrency(values[fields.denominator])
+    // [fields.name.username]: validateEmail(values[fields.name.username]),
+  })
 }
 
 const DenominatorForm = reduxForm(formConfig)(Denominator)
 
-const DenominatorPage = (props) => {
+const DenominatorPage = ({value, ...props}) => {
   return (
-    <div className="App">
-      <DenominatorForm/>
+    <div className="container">
+      {
+        !isEmptyOrNull(value) && <Display value={value}/>
+      }
+    <div className="denomField">
+      <DenominatorForm {...props}/>
+    </div>
     </div>
   );
 }
 
-export default DenominatorPage;
+const denomState = ({value}) => ({
+  value,
+})
+
+const denomDispatch = null;
+
+const ConnectedDenominator = connect(denomState, denomDispatch)(DenominatorPage)
+
+export default ConnectedDenominator;
