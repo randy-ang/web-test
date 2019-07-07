894c1fb9de495ea6c3151f1f643c095570034437
randy-ang <randyanggadinata@gmail.com>
2019-07-07 12:57:38 +0700

    added test versions

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,1 +1,2 @@
-cp ../.env.development ../.env
+#!/usr/bin/env bash
+cd C:/web-test && cp "$PWD/.env.development" "$PWD/.env"&& echo "copying development .env" || echo "fail to copy development .env"
