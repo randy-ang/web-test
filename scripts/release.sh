596057775255422c1d27bce021a56e0319db7930
randy-ang <randyanggadinata@gmail.com>
2019-07-07 11:12:43 +0700

    push stuff

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-cp "$PWD/.env.production" "$PWD/.env"&& echo "copying production .env" || echo "fail to copy production .env"
+cd C:/web-test && cp "$PWD/.env.production" "$PWD/.env"&& echo "copying production .env" || echo "fail to copy production .env"
